2a2d4a7a73f8f6da2ea76cbd9571fa77e8200b0f
Ray Anthony <rgarcia@gilesparscale.com>
2018-04-30 14:55:28 -0500

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) console.log('Unable to connect to MongoDB server');
+
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({ _id: new ObjectID('5ae76837b7298b163fafdb13')}).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({
+        name: 'Ray'
+    }).toArray().then(docs => {
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, err => {
+        console.log('No documents found', err);
+    });
+
+
+    // client.close();
+});
